3cba006be17eedc0eb5085329fb4ed8d005baefc
foleya <foley.ah@gmail.com>
2020-03-15 20:14:30 -0400

    practiced making a line graph

--- src/Components/Graph.js
+++ src/Components/Graph.js
@@ -1,25 +1,54 @@
 import React, { useEffect, useRef, useState } from "react";
-import { curveCardinal, line, select } from "d3";
-import killcountcsv from "../Data/nilheimkillcount.csv";
+import {
+  axisBottom,
+  axisRight,
+  curveCardinal,
+  line,
+  max,
+  select,
+  scaleLinear
+} from "d3";
+// import killcountcsv from "../Data/nilheimkillcount.csv";
 
 const Graph = () => {
-  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75, 40]);
 
   const svgRef = useRef();
 
   useEffect(() => {
     const svg = select(svgRef.current);
 
+    const xScale = scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, 300]);
+
+    const yScale = scaleLinear()
+      .domain([0, max(data)])
+      .range([150, 0]);
+
+    const xAxis = axisBottom(xScale).ticks(data.length);
+    svg
+      .select(".x-axis")
+      .style("transform", "translateY(150px)")
+      .call(xAxis);
+
+    const yAxis = axisRight(yScale);
+    svg
+      .select(".y-axis")
+      .style("transform", "translateX(300px)")
+      .call(yAxis);
+
     const myLine = line()
-      .x((value, index) => index * 50)
-      .y(value => 150 - value)
+      .x((value, index) => xScale(index))
+      .y(yScale)
       .curve(curveCardinal);
 
     svg
-      .selectAll("path")
+      .selectAll(".line")
       .data([data])
       .join("path")
-      .attr("d", value => myLine(value))
+      .attr("class", "line")
+      .attr("d", myLine)
       .attr("fill", "none")
       .attr("stroke", "steelblue");
     // Circle Example
@@ -35,7 +64,10 @@ const Graph = () => {
 
   return (
     <React.Fragment>
-      <svg ref={svgRef}></svg>
+      <svg ref={svgRef}>
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
       <br />
       <button
         onClick={() => setData(data.map(value => value + Math.random() * 10))}
