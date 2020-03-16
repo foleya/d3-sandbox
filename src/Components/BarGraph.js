9a7825ffbbc2586190d84d5907784a5d6f148ca2
foleya <foley.ah@gmail.com>
2020-03-16 13:56:33 -0400

    delete comments

--- src/Components/BarGraph.js
+++ src/Components/BarGraph.js
@@ -3,46 +3,13 @@ import { axisBottom, axisLeft, max, scaleLinear, scaleBand, select } from "d3";
 import funDataJson from "../Data/nilheimFunCount.json";
 
 const BarGraph = () => {
-  const [data, setData] = useState(funDataJson);
+  const data = funDataJson;
 
   const svgRef = useRef();
 
-  const proportions = {
-    width: 500,
-    height: 310,
-    margin: { top: 0, right: 0, bottom: 30, left: 30 }
-  };
   const width = 500;
   const height = 310;
   const margin = { top: 0, right: 0, bottom: 30, left: 30 };
-
-  //   const xValue = d => d.murders;
-  //   const yValue = d => d.player;
-
-  //   const xScale = scaleLinear()
-  //     .domain([0, max(data, xValue)])
-  //     .range([0, width]);
-  //   const xAxis = axisBottom().scale(xScale);
-  //   // .tickFormat(d3.timeFormat("%b"));
-  //   const yScale = scaleBand()
-  //     .domain(data.map(yValue))
-  //     .range([0, height])
-  //     .padding([0.3]);
-  //   const yAxis = axisLeft().scale(yScale);
-  //   // .tickFormat(d => `${d}℉`);
-  //   console.log(xAxis, yAxis);
-  //   let bars = [];
-  //   data.forEach(d => {
-  //     console.log(xScale(xValue(d)));
-  //     bars.push({
-  //       player: d.player,
-  //       murders: +d.murders,
-  //       y: yScale(yValue(d)),
-  //       width: xScale(xValue(d)),
-  //       height: yScale.bandwidth()
-  //     });
-  //   });
-  //   setBars(bars);
 
   useEffect(() => {
     const svg = select(svgRef.current);
@@ -54,7 +21,7 @@ const BarGraph = () => {
     // SCALES
     const xScale = scaleLinear()
       .domain([0, 6]) // max function here!
-      .range([0, proportions.width]);
+      .range([0, width]);
 
     const yScale = scaleBand()
       .domain(data.map(yValue))
@@ -85,7 +52,7 @@ const BarGraph = () => {
       .attr("height", yScale.bandwidth())
       .attr("y", d => yScale(yValue(d)))
       .attr("fill", "steelblue");
-  }, [data, proportions]);
+  }, [data]);
 
   return (
     <React.Fragment>
